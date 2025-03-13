9361a5acf5672b1a9358fbe5cc11cdd3f3f8cd1c
Tim Graupmann <tgraupmann@gmail.com>
2025-03-12 18:05:25 -0700

    Initial commit of project

---
+++ Razer/WyvrnSDKTypes.h
@@ -0,0 +1,30 @@
+﻿//! \file WyvrnSDKTypes.h
+//! \brief Data types.
+
+#ifndef _WYVRNSDKTYPES_H_
+#define _WYVRNSDKTYPES_H_
+
+#pragma once
+
+#define LEAN_AND_MEAN
+#include <Windows.h>
+
+typedef LONG            RZRESULT;           //!< Return result.
+
+namespace WyvrnSDK
+{
+    typedef struct APPINFOTYPE
+    {
+        wchar_t Title[256];
+        wchar_t Description[1024];
+        struct Author
+        {
+            wchar_t Name[256];
+            wchar_t Contact[256];
+        } Author;
+        const DWORD SupportedDevice = 63;
+        DWORD Category;
+    } APPINFOTYPE;
+}
+
+#endif
